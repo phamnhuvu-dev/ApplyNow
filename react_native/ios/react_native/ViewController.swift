2c2da0c69cc09b56d176c0feaa6a9f75cedd6f07
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2018-08-22 13:21:39 +0700

    [RN-iOS] change Obj-C to Swift base project

---
+++ react_native/ios/react_native/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  react_native
+//
+//  Created by Phạm Như Vũ on 8/22/18.
+//  Copyright © 2018 Phạm Như Vũ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
