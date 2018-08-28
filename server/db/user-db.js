82da3fc5ab66df8b56af91fb116590ef25dc11b0
Phạm Như Vũ <phamnhuvu.dev@gmail.com>
2018-08-28 18:02:33 +0700

    no message

--- server/db/user-db.js
+++ server/db/user-db.js
@@ -5,19 +5,34 @@ const User = (sequelize) => {
       name: {type: Sequelize.STRING},
       email: {type: Sequelize.STRING},
       password: {type: Sequelize.STRING},
+      description: {type: Sequelize.STRING}
     }
   );
 
   User.sync({force: true}).then(() => {
-    // Table created
-    // return User.create({
-    //   firstName: 'John',
-    //   lastName: 'Hancock'
-    // });
     return User.create({
       name: "Phạm Như Vũ",
       email: "phamnhuvu.ou@gmail.com",
-      password: "123456789"
+      password: "123456789",
+      description: "Mobile developer"
+    });
+  });
+
+  User.sync({force: true}).then(() => {
+    return User.create({
+      name: "Đỗ Thị Thu Hằng",
+      email: "dothithuhang@gmail.com",
+      password: "123456789",
+      description: "QA/QC"
+    });
+  });
+
+  User.sync({force: true}).then(() => {
+    return User.create({
+      name: "Trịnh Ngọc Tiến",
+      email: "trinhngoctien@gmail.com",
+      password: "123456789",
+      description: "Web developer"
     });
   });
 
